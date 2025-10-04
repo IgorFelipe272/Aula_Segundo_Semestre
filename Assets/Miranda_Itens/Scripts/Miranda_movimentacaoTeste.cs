ffc81f08108a7329091f89055bb4061860dd7581
G-Mirand <130694211+G-Mirand@users.noreply.github.com>
2025-10-04 13:21:10 -0300

    asda

---
+++ Assets/Miranda_Itens/Scripts/Miranda_movimentacaoTeste.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using System.Collections.Generic;
+using UnityEngine.AI;
+
+public class Miranda_movimentacaoTeste : MonoBehaviour
+{
+    public List<Transform> wayPoint;
+
+    public NavMeshAgent agent;
+
+    public int currentWayPointIndex = 0;
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Walking();
+    }
+
+    public void Walking()
+    {
+        if(wayPoint.Count == 0)
+        {
+            return;
+        }
+
+        float distanceToWayPoint = Vector3.Distance(wayPoint[currentWayPointIndex].position, transform.position);
+
+        if(distanceToWayPoint <= 3)
+        {
+            currentWayPointIndex = (currentWayPointIndex + 1) % wayPoint.Count;
+        }
+
+        agent.SetDestination(wayPoint[currentWayPointIndex].position);
+    }
+}
